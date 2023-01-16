6d138128791a8dbd99f1624aad8eb9a6bae8aa85
Dawlau <blahoviciandrei1@gmail.com>
2023-01-16 14:30:25 +0100

    made deepwalk and im work

--- Crosswalk/deepwalk/run.sh
+++ Crosswalk/deepwalk/run.sh
@@ -17,14 +17,14 @@ Phom=0.025
 
 
 for i in ''; do #'2' '3' '4' '5'; do
-	for Phet in 0.001 0.005 0.01 0.015; do # 0.001 0.005 0.01 0.015 0.02 0.025; do
-		filename=synthetic/synthetic_n${nodes}_Pred${Pred}_Phom${Phom}_Phet${Phet}
+	for Phet in 0.001; do #0.005 0.01 0.015; do # 0.001 0.005 0.01 0.015 0.02 0.025; do
+		filename=~/FACT-AI/Crosswalk/data/synth2/synthetic_n${nodes}_Pred${Pred}_Phom${Phom}_Phet${Phet}
 		#method='unweighted'
-		#outfile=${filename}.embeddings_${method}_d${d}_$i 
+		#outfile=${filename}.embeddings_${method}_d${d}_$i
 		#echo ${i}'   '$method
 		#python deepwalk --format edgelist --input ${filename}.links --max-memory-data-size 0 --number-walks 80 --representation-size $d --walk-length 40 --window-size 10 --workers 30 --output $outfile --weighted $method --sensitive-attr-file ${filename}.attr
 		for rwl in 5; do #5 10 20; do
-			for bndry in 0.1 0.5 0.9; do #0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do #0.2 0.5 0.7 0.9; do
+			for bndry in 0.1; do # 0.5 0.9; do #0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do #0.2 0.5 0.7 0.9; do
 				for exponent in '5.0'; do #'2.0' '5.0' '8.0'; do #1.0 1.25 1.5 1.75 2.0 4.0 6.0 8.0; do #0.5 1.0 2.0; do
 					for bndrytype in 'bndry'; do # 'revbndry'; do
 						method='random_walk_'${rwl}'_'${bndrytype}'_'${bndry}'_exp_'${exponent}
@@ -78,7 +78,7 @@ done
 #for dataset in 'rice'; do # 'sample_1000' 'sample_4000'; do
 #	python deepwalk --format edgelist --input data/${dataset}/${dataset}.links --max-memory-data-size 0 --number-walks 80 --representation-size $d --walk-length 40 --window-size 10 --workers 30 --output data/${dataset}/${dataset}.embeddings_random_d$d --weighted random --sensitive-attr-file data/${dataset}/${dataset}.attr
 #for psc in 0.9 0.7 0.5 0.2; do
-#	echo $dataset	
+#	echo $dataset
 #	python deepwalk --format edgelist --input data/${dataset}/${dataset}.links --max-memory-data-size 0 --number-walks 80 --representation-size $d --walk-length 40 --window-size 10 --workers 30 --output data/${dataset}/${dataset}.embeddings_smartshortcut_${psc}_d$d --weighted smartshortcut_${psc} --sensitive-attr-file data/${dataset}/${dataset}.attr
 #done
 #	python deepwalk --format edgelist --input data/${dataset}/${dataset}.links --max-memory-data-size 0 --number-walks 80 --representation-size $d --walk-length 40 --window-size 10 --workers 30 --output data/${dataset}/${dataset}.embeddings_prb_${prb}_pbr_${pbr}_d$d --weighted prb_${prb}_pbr_${pbr} --sensitive-attr-file data/${dataset}/${dataset}.attr
